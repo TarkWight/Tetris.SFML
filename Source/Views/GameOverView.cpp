8f314eee2d1b867f0e51966ef272e2020b209c95
Tark_Wight <arsentiy.gonda@gmail.com>
2024-09-23 21:59:26 +0700

    [fix] GameOverView - score list

--- Source/Views/GameOverView.cpp
+++ Source/Views/GameOverView.cpp
@@ -22,10 +22,28 @@ GameOverView::GameOverView(const std::function<void()>& onRestart, const std::fu
 
 void GameOverView::draw(sf::RenderWindow& window, const std::string& playerName, int currentScore, const std::vector<ScoreEntry>& leaderboard) {
     window.draw(background);
+
+    gameOverText.setCharacterSize(50);
+    gameOverText.setPosition(window.getSize().x / 2 - gameOverText.getGlobalBounds().width / 2, 100);
+
     window.draw(gameOverText);
+
     scoreText.setString("Score: " + std::to_string(currentScore));
+    scoreText.setCharacterSize(30); 
+    scoreText.setPosition(window.getSize().x / 2 - scoreText.getGlobalBounds().width / 2, 200);
     window.draw(scoreText);
 
+    restartText.setCharacterSize(30); 
+    restartText.setPosition(window.getSize().x / 2 - restartText.getGlobalBounds().width / 2, 300);
     window.draw(restartText);
+
+    exitText.setCharacterSize(30);
+    exitText.setPosition(window.getSize().x / 2 - exitText.getGlobalBounds().width / 2, 370);
     window.draw(exitText);
+
+    restartButton.setPosition(window.getSize().x / 2 - restartButton.getSize().x / 2, 300);
+    exitButton.setPosition(window.getSize().x / 2 - exitButton.getSize().x / 2, 370);
+
+    window.draw(restartButton);
+    window.draw(exitButton);
 }
